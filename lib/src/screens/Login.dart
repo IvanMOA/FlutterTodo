cdb664ce2055862a2dde4cc427009684489e1b8f
IvanMOA <52722893+IvanMOA@users.noreply.github.com>
2021-01-22 18:46:08 -0600

    Created a simple form in the Login screen

--- lib/src/screens/Login.dart
+++ lib/src/screens/Login.dart
@@ -1,7 +1,35 @@
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+
   Widget build(BuildContext context) {
-    return Text("LoginPage");
+    return Container(
+        margin: EdgeInsets.only(right: 50, left: 50),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                decoration: InputDecoration(hintText: "Enter your email"),
+                validator: (email) {
+                  if (email == '') {
+                    return 'Please enter an email';
+                  }
+                  return null;
+                },
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 16),
+                child: RaisedButton(
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {}
+                    },
+                    child: Text("Sign in")),
+              )
+            ],
+          ),
+        ));
   }
 }
