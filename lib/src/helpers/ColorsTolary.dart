e6e78e1e5b4eead99ce7cb8b30607c8d2d088a74
IvanMOA <52722893+IvanMOA@users.noreply.github.com>
2021-02-01 15:16:26 -0600

    Added color palette

---
+++ lib/src/helpers/ColorsTolary.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorsTolary {
+  static const Color TolaryGreen = Color(0xFF1B998B);
+  static const Color TolaryPink = Color(0xFFED217C);
+  static const Color TolaryBlack = Color(0xFF2D3047);
+  static const Color TolaryYellow = Color(0xFFFFFD82);
+  static const Color TolaryGray = Color(0xFFE3DFFF);
+}
