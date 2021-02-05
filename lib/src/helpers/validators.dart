f5769ff35c2d210e644bce7817e28dea55448483
IvanMOA <52722893+IvanMOA@users.noreply.github.com>
2021-02-04 18:10:21 -0600

    Added abstract class Validator to use it as an interface

--- lib/src/helpers/validators.dart
+++ lib/src/helpers/validators.dart
@@ -1,10 +1,14 @@
-class EmailValidator {
+abstract class Validator {
+  static validate(String value) {}
+}
+
+class EmailValidator implements Validator {
   static String validate(String value) {
     return value.isEmpty ? "Email can't be empty" : null;
   }
 }
 
-class PasswordValidator {
+class PasswordValidator implements Validator {
   static String validate(String value) {
     return value.isEmpty ? "Password can't be empty" : null;
   }
