94d8dc4ac21894ba0492a7a4875b17a9abb944bc
IvanMOA <52722893+IvanMOA@users.noreply.github.com>
2021-02-09 16:00:08 -0600

    Added validators for signup fields

--- lib/src/helpers/validators.dart
+++ lib/src/helpers/validators.dart
@@ -15,3 +15,15 @@ class PasswordValidator implements Validator {
     return value.isEmpty ? "Password can't be empty" : null;
   }
 }
+
+class ConfirmationPasswordValidator implements Validator {
+  static String validate(String password, String confirmationPassword) {
+    return password != confirmationPassword ? "Password doesn't match" : null;
+  }
+}
+
+class DisplayNameValidator implements Validator {
+  static String validate(String value) {
+    return value.isEmpty ? "Your must enter your name" : null;
+  }
+}
